e0087383d070107f36283761db02c8db093a0de1
DarthVaderOP <bishtlalita810@gmail.com>
2024-11-25 08:52:33 +0530

    Add files via upload

---
+++ KBC.py
@@ -0,0 +1,27 @@
+print('WELCOME TO KAUN BANEGA CROREPATI')
+input("Press enter to continue")
+print ("Q1.) Red is a ___ ")
+print ("(a)Colour           (b)Fruit                (c)Vegetable                 (d)Human ")
+ans_1 = input("Your ans is :")
+if ans_1=="a" or ans_1=="A":  
+   print("Correct answer. You have won 10000 rupees.")
+else:
+   print("Sorry, wrong ans, you lost so pls get lost.") 
+   exit()
+print("Q2.) 2+2=___")
+print(" (a)1            (b)2               (c)3             (d)4 ")
+ans_2=input("Your ans is :")
+if ans_2=="d" or ans_2=="D":
+   print("Correct answer. You have won 20000 rupees. ")
+else :
+   print("Sorry, wrong ans, go home with 5000 rupees.")
+   exit()
+print("Q3.) Integral of x is ___")
+print("(a)x^2 + C           (b)x^2/2 + C          (c)1             (d)C")
+ans_3=input("Your ans is :")
+if ans_3=="b" or ans_3=="B":
+   print("Correct answer. You have won 30000 rupees. You can go home now.")
+else :
+   print("Sorry, wrong ans, go home with 15000 rupees.")
+
+   
